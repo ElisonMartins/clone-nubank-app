c1f0629dc728ac06a5a4675cb5bdf21b7a2be5d3
elisonmartins <martinselison10@gmail.com>
2023-08-08 21:50:14 -0300

    feat: Adicionando melhorias

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { StyleSheet, View, Text, FlatList, Dimensions } from 'react-native';
+import { StyleSheet, View, Text, FlatList, Dimensions, ScrollView } from 'react-native';
 import { AntDesign, Feather, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { Pix, BarChart, MonetizationOn, Send, AccountBalance, Description, AddCircleOutline } from '@mui/icons-material';
-//import { Container, Box } from '@mui/material';
 
 const windowWidth = Dimensions.get('window').width;
 
@@ -41,6 +40,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
+    paddingHorizontal: 20,
   },
   infoContainer: {
     backgroundColor: 'white',
@@ -82,6 +82,7 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     borderBottomWidth: 1,
     borderBottomColor: '#848389',
+    marginVertical: 8,
   },
   sectionTextContainer: {
     flex: 1,
@@ -115,8 +116,6 @@ const styles = StyleSheet.create({
   },
 });
 
-
-
 export default function App() {
   const data = [
     { icon: <Pix />, text: 'Área Pix' },
@@ -146,34 +145,34 @@ export default function App() {
       <View style={styles.titleContainer}>
         <Text style={styles.greeting}>Olá, Elison</Text>
       </View>
-      <View style={styles.whiteView}>
+      <ScrollView style={styles.whiteView}>
         <View style={styles.infoContainer}>
           <Text style={styles.infoText}>Conta</Text>
           <Text style={styles.infoText}>R$ 3.156,94</Text>
         </View>
         <FlatList
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        data={data}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.circleContainer}>
-            <View style={styles.circle}>
-              {item.icon}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={data}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.circleContainer}>
+              <View style={styles.circle}>
+                {item.icon}
+              </View>
+              <Text style={styles.iconText}>{item.text}</Text>
             </View>
-            <Text style={styles.iconText}>{item.text}</Text>
-          </View>
-        )}
-        contentContainerStyle={styles.sliderContainer}
-        snapToInterval={windowWidth * 0.3 + 20}
-        decelerationRate="fast"
-        getItemLayout={(data, index) => ({
-          length: windowWidth * 0.3 + 20,
-          offset: (windowWidth * 0.3 + 20) * index,
-          index,
-        })}
-      />
-        <View style={styles.sectionContainer}>
+          )}
+          contentContainerStyle={styles.sliderContainer}
+          snapToInterval={windowWidth * 0.3 + 20}
+          decelerationRate="fast"
+          getItemLayout={(data, index) => ({
+            length: windowWidth * 0.3 + 20,
+            offset: (windowWidth * 0.3 + 20) * index,
+            index,
+          })}
+        />
+        <View style={[styles.sectionContainer, { marginTop: 20, marginBottom: 20 }]}>
           <View style={styles.sectionTextContainer}>
             <Text style={styles.sectionTitle}>Meus cartões</Text>
           </View>
@@ -194,52 +193,73 @@ export default function App() {
             </View>
           )}
         />
-      </View>
-      <View style={styles.sectionContainer}>
-        <View style={styles.sectionTextContainer}>
-          <Text style={styles.sectionTitle}>Empréstimo</Text>
-          <View style={styles.sectionLine}></View>
-          <Text style={styles.sectionSubtitle}>Valor disponível de até</Text>
-          <Text style={styles.sectionSubtitle}>R$ 25.000,00</Text>
+        <View style={styles.sectionContainer}>
+          <View style={styles.sectionTextContainer}>
+            <Text style={styles.sectionTitle}>Empréstimo</Text>
+            <View style={styles.sectionLine}></View>
+            <Text style={styles.sectionSubtitle}>Valor disponível de até</Text>
+            <Text style={styles.sectionSubtitle}>R$ 25.000,00</Text>
+          </View>
+          <View style={styles.sectionIcon}>
+            <AntDesign name="right" size={24} color="#000" />
+          </View>
         </View>
-        <View style={styles.sectionIcon}>
-          <AntDesign name="right" size={24} color="#000" />
+        <View style={styles.sectionContainer}>
+          <View style={styles.sectionTextContainer}>
+            <Text style={styles.sectionTitle}>Seguros</Text>
+            <View style={styles.sectionLine}></View>
+            <Text style={styles.sectionSubtitle}>Proteção para você cuidar do que importa</Text>
+          </View>
         </View>
-      </View>
-
-      <View style={styles.sectionContainer}>
-        <View style={styles.sectionTextContainer}>
-          <Text style={styles.sectionTitle}>Seguros</Text>
-          <View style={styles.sectionLine}></View>
-          <Text style={styles.sectionSubtitle}>Proteção para você cuidar do que importa</Text>
+        {/* Subseção dentro de Seguros - Seguro Vida */}
+        <View style={[styles.sectionContainer, { flexDirection: 'row' }]}>
+          <View style={styles.sectionTextContainer}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <AntDesign name="hearto" size={24} color="#830AD1" style={{ marginRight: 10 }} />
+              <Text style={[styles.sectionTitle, { color: '#830AD1' }]}>Seguro Vida</Text>
+            </View>
+          </View>
+          <View style={[styles.sectionIcon, { marginRight: 20 }]}>
+            <Text style={{ color: '#830AD1', fontSize: 18 }}>Conhecer</Text>
+          </View>
         </View>
-      </View>
-      
-      <View style={styles.sectionContainer}>
-        <View style={styles.sectionTextContainer}>
-          <Text style={styles.sectionTitle}>Investimentos</Text>
-          <View style={styles.sectionLine}></View>
-          <Text style={styles.sectionSubtitle}>O jeito Nu de investir:</Text>
-          <Text style={styles.sectionSubtitle}>sem asteriscos, linguagem fácil</Text>
-          <Text style={styles.sectionSubtitle}>e a partir de R$1. Saiba mais.</Text>
+        {/* Subseção dentro de Seguros - Seguro de Celular */}
+        <View style={[styles.sectionContainer, { flexDirection: 'row' }]}>
+          <View style={styles.sectionTextContainer}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Ionicons name="ios-phone-portrait-outline" size={24} color="#F08E29" style={{ marginRight: 10 }} />
+              <Text style={[styles.sectionTitle, { color: '#F08E29' }]}>Seguro de Celular</Text>
+            </View>
+          </View>
+          <View style={[styles.sectionIcon, { marginRight: 20 }]}>
+            <Text style={{ color: '#F08E29', fontSize: 18 }}>Conhecer</Text>
+          </View>
         </View>
-        <View style={styles.sectionIcon}>
-          <AntDesign name="right" size={24} color="#000" />
+        <View style={styles.sectionContainer}>
+          <View style={styles.sectionTextContainer}>
+            <Text style={styles.sectionTitle}>Investimentos</Text>
+            <View style={styles.sectionLine}></View>
+            <Text style={styles.sectionSubtitle}>O jeito Nu de investir:</Text>
+            <Text style={styles.sectionSubtitle}>sem asteriscos, linguagem fácil</Text>
+            <Text style={styles.sectionSubtitle}>e a partir de R$1. Saiba mais.</Text>
+          </View>
+          <View style={styles.sectionIcon}>
+            <AntDesign name="right" size={24} color="#000" />
+          </View>
         </View>
-      </View>
-
-      <View style={styles.sectionContainer}>
-        <View style={styles.sectionTextContainer}>
-          <Text style={styles.sectionTitle}>Shopping</Text>
-          <View style={styles.sectionLine}></View>
-          <Text style={styles.sectionSubtitle}>
-            Vantagens exclusivas das nossas marcas preferidas
-          </Text>
+        <View style={styles.sectionContainer}>
+          <View style={styles.sectionTextContainer}>
+            <Text style={styles.sectionTitle}>Shopping</Text>
+            <View style={styles.sectionLine}></View>
+            <Text style={styles.sectionSubtitle}>
+              Vantagens exclusivas das nossas marcas preferidas
+            </Text>
+          </View>
+          <View style={styles.sectionIcon}>
+            <AntDesign name="right" size={24} color="#000" />
+          </View>
         </View>
-        <View style={styles.sectionIcon}>
-          <AntDesign name="right" size={24} color="#000" />
-        </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
